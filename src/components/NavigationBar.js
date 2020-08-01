177d47fdce88b5ea6328049261a3d9401fa03602
Navid Hariri <navidhariri25@gmail.com>
2020-07-31 21:50:06 -0700

    put in a few components and working on making testing work

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function NavigationBar() {
+    return (
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda dicta dolor ducimus error fuga in magnam
+            modi, molestias non qui quia quisquam recusandae rem sed sunt vero voluptate? Accusamus aliquid asperiores
+            assumenda beatae delectus dolorem eius in ipsa iusto magnam, maiores mollitia nobis non numquam obcaecati
+            odio pariatur quae quas quo saepe sed sequi similique sunt tenetur voluptatibus? A ad aliquam amet animi
+            asperiores aspernatur at, beatae dolor eius est fugit harum in iusto molestiae mollitia nam, natus nulla
+            officiis perspiciatis placeat quia repellat saepe sed tempora, tempore veniam voluptate! A, ab alias in
+            laudantium neque possimus qui tenetur voluptates.</p>
+    );
+}
+
+export default NavigationBar();
